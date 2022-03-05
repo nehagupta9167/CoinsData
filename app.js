b9be5099fc2b1b177f329d50c509aaa7561c5a0d
nehagupta9167 <49072103+nehagupta9167@users.noreply.github.com>
2022-03-04 19:36:04 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+const port = 5000;
+
+// Static Files
+app.use(express.static('public'))
+//app.use(express.static(path.join(__dirname, 'public')));
+
+// Templating Engine
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+app.use(express.json());
+// Parsing middleware
+// Parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false })); // Deprecated
+app.use(express.urlencoded({ extended: true })); // New
+
+
+
+// Routes
+const indexRouter = require('./src/routes/index.js');
+
+app.use('/', indexRouter);
+//app.use('/article', newsRouter);
+
+// Listen on port 5000
+app.listen(port, () => console.log(`Listening on port ${port}`));
